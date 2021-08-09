920d2b0d920509609e531fa9c2097bcf3f67cb88
ArtyomKorn1lov <temka.kornilov@mail.ru>
2021-08-09 16:29:53 +0300

    Fixed Entities
    -Fixed Entities

--- Cookers/Core/Entity/Recipe.cs
+++ Cookers/Core/Entity/Recipe.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Entity
+namespace Domain.Entity
 {
     public class Recipe : Entity
     {
@@ -13,7 +13,7 @@ namespace Core.Entity
         public string Photo { get; set; }
         public int CookingTime { get; set; }
         public int Peoples { get; set; }
-        public int Favorites { get; set; }
+        public int Favourites { get; set; }
         public int Likes { get; set; }
         public int IngredientId { get; set; }
         public int StepId { get; set; }
