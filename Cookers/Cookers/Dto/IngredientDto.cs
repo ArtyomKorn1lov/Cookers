c69a826b653d63beeb404771a7b075f85f4b2897
ArtyomKorn1lov <temka.kornilov@mail.ru>
2021-08-11 21:32:44 +0300

    Fixed bugs
    Fixed bugs

--- Cookers/Cookers/Dto/IngredientDto.cs
+++ Cookers/Cookers/Dto/IngredientDto.cs
@@ -4,7 +4,8 @@ namespace Web.Dto
 {
     public class IngredientDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
-        public List<string> Description { get; set; }
+        public string Description { get; set; }
     }
 }
