78376dc4e09af6478f5085f66bd88b045431ee3b
ArtyomKorn1lov <temka.kornilov@mail.ru>
2021-08-12 16:39:34 +0300

    Add comment
    Add comment

--- Cookers/Cookers/Startup.cs
+++ Cookers/Cookers/Startup.cs
@@ -25,7 +25,7 @@ namespace Web
 
             services.AddDbContext<RecipeDbContext>( options =>
             {
-                string connectionString = @"Data Source=DUKE\SQLEXPRESS; Initial Catalog=cookers; Pooling=true; Integrated Security=SSPI";
+                string connectionString = @"Data Source=DUKE\SQLEXPRESS; Initial Catalog=cookers; Pooling=true; Integrated Security=SSPI"; //Костыль
                 options.UseSqlServer( connectionString );
             } );
         }
